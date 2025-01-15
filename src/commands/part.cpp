ac11bee8fd5475f5fc64af049b861742d740be4b
Damien Lebranchu <dalebran@student.42lehavre.fr>
2025-01-15 16:04:42 +0100

    +: validatePart()

--- src/commands/part.cpp
+++ src/commands/part.cpp
@@ -2,6 +2,22 @@
 #include "Tokenisation.hpp"
 
 bool Server::validatePart(const TokenisedCommand &cmd, int fd) {
+  if (cmd.getArguments().size() < 1) {
+    std::cerr << "error_461" << std::endl;
+    return false;
+  }
+
+  std::string channelName = cmd.getArguments()[0];
+  if (_channels.getChannel(channelName) == NULL) {
+    std::cerr << "error_403 " << channelName << std::endl;
+    return false;
+  }
+
+  if (_channels.isUserInChannel(channelName, fd)) {
+    std::cerr << "error_442 " << channelName << std::endl;
+    return false;
+  }
+
   (void)cmd;
   (void)fd;
   return true;
