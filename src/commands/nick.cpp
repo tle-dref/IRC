1cce0c3a4d954895e94b596a4f5f867ed751d905
Damien Lebranchu <dalebran@student.42lehavre.fr>
2025-01-16 23:24:38 +0100

    +: notify on NICK change

--- src/commands/nick.cpp
+++ src/commands/nick.cpp
@@ -9,11 +9,14 @@ bool Server::validateNick(const TokenisedCommand &cmd, int fd) {
     error_431(fd, _clients.getClientname(fd));
     return false;
   }
+
   const std::string &nickname = cmd.getArguments()[0];
-  if (nickname.size() > 9) {
+
+  if (nickname[0] == '#' || nickname.size() > 9) {
     error_432(fd, _clients.getClientname(fd), nickname);
     return false;
   }
+
   if (_clients.nicknameExists(nickname)) {
     error_433(fd, _clients.getClientname(fd), nickname);
     return false;
@@ -29,6 +32,16 @@ void Server::doNick(const TokenisedCommand &cmd, int fd) {
   std::string nick = cmd.getArguments()[0];
   std::string msg =
       ":" + _clients.getClient(fd)->nickname + "!@GLMRC NICK " + nick + "\n";
+
+  const std::map<std::string, Channel *> &channels = _channels.getChannels();
+  for (std::map<std::string, Channel *>::const_iterator it = channels.begin();
+       it != channels.end(); ++it) {
+    const std::string &channelName = it->first;
+    if (_channels.isUserInChannel(channelName, fd)) {
+      _channels.notifyChannel(msg, channelName);
+    }
+  }
+
   _clients.getClient(fd)->nickname = nick;
   send(fd, msg.c_str(), msg.size(), MSG_NOSIGNAL);
 }
